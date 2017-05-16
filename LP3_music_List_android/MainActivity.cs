00e651a84376ccb91befd408c6a0115d61cdfbf5
Grzegorz Duraj <grzegorzduraj@outlook.com>
2017-05-16 20:45:23 +0200

    Add project files.

---
+++ LP3_music_List_android/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace LP3_music_List_android
+{
+    [Activity(Label = "LP3_music_List_android", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
