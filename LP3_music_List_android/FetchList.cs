9a4f534c06490a4b2384f8a41e6f7ff4bb328fc9
Greg D <stefan.power@gmail.com>
2017-05-18 22:05:44 +0100

    try to debug

--- LP3_music_List_android/FetchList.cs
+++ LP3_music_List_android/FetchList.cs
@@ -5,18 +5,19 @@ using System.Net;
 using System.Json;
 using System.IO;
 
-
+using System.Collections.Generic;
 using Android.App;
 
 namespace LP3_music_List_android
 {
     class FetchList : ListActivity
     {
+        static List<string> listOfSongs; //= new List<string>();
 
-
-        void GetSite(string urlToGet = null)
+        internal void GetSite(string urlToGet = null)
         {
-            string lp3Url; 
+            Console.WriteLine("GRDU FetchList::GetSite() ");
+            string lp3Url;
 
             if (urlToGet == null)
             {
@@ -24,16 +25,16 @@ namespace LP3_music_List_android
             }
             else
             {
-                lp3Url = urlToGet; 
+                lp3Url = urlToGet;
             }
 
             var httpReq = (HttpWebRequest)HttpWebRequest.Create(new Uri(lp3Url));
             httpReq.BeginGetResponse(new AsyncCallback(ResponseCallBack), httpReq);
         }
 
-
-        void ResponseCallBack (IAsyncResult ar )
+        void ResponseCallBack(IAsyncResult ar)
         {
+            Console.WriteLine("GRDU FetchList::ResponseCallBack() ");
             HttpWebRequest httpReq = (HttpWebRequest)ar.AsyncState;
 
             using (var httpRes = (HttpWebResponse)httpReq.EndGetResponse(ar))
@@ -45,21 +46,32 @@ namespace LP3_music_List_android
 
         void ParseResult(HttpWebResponse httpRes)
         {
+            Console.WriteLine("GRDU FetchList::ParseResult() ");
+            Console.WriteLine ("GRDU " + httpRes.ContentLength);
             Stream s = httpRes.GetResponseStream();
+            s.ToString();
+            Console.WriteLine("GRDU FetchList::ParseResult() 2");
+            JsonObject.Load(s);
+            Console.WriteLine("GRDU FetchList::ParseResult() 2.1");
             JsonObject jsonObject = (JsonObject)JsonObject.Load(s);
-            
 
-            var results = (from result in (JsonArray ) jsonObject ["result"] let jResult = result as JsonObject select jResult["text"].ToString ()).ToArray();
-           
-            RunOnUiThread(() => {
-               PopulateLp3List(results);
-           });
+            Console.WriteLine("GRDU FetchList::ParseResult() 3");
+
+            var results = (from result in (JsonArray)jsonObject["result"] let jResult = result as JsonObject select jResult["text"].ToString()).ToArray();
+
+            RunOnUiThread(() =>
+            {
+                PopulateLp3List(results);
+            });
 
 
         }
-        
-        void PopulateLp3List (string [] results)
+
+        void PopulateLp3List(string[] results)
         {
+            Console.WriteLine("GRDU FetchList::PopulateLp3List() ");
+            //  results.
+            listOfSongs = new List<string>(results);
             //ListAdapter = new ArrayAdapter<string>(this, Resource.Layout.ItemView, results);
         }
     }
--- LP3_music_List_android/MainActivity.cs
+++ LP3_music_List_android/MainActivity.cs
@@ -1,24 +1,29 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using System;
 
 namespace LP3_music_List_android
 {
     [Activity(Label = "LP3_music_List_android", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+
+        FetchList fetchList = new FetchList();
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-
+            Console.WriteLine("GRDU MainActivity OnCreate()");
             // Set our view from the "main" layout resource
             SetContentView (Resource.Layout.Main);
 
-            Button fetchList = this.FindViewById<Button>(Resource.Id.FetchListButton);
+            Button fetchListButton = this.FindViewById<Button>(Resource.Id.FetchListButton);
 
-
+            fetchList.GetSite();
           
         }
     }
 }
 
+//$ nox_adb connect 127.0.0.1:62001
